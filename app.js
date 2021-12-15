846a8aa3e0fe0078510a10f71952e1c3813698dd
Tom Southwick <thomas.h.southwick@gmail.com>
2021-12-15 11:19:17 +1100

    incorporating front end to API routes + fixing up some other issues for re-submission

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 var port = process.env.PORT || 8080;
 var fs = require("fs");
+app.use(express.static("public"));
 
 app.use(express.json());
 let messages = [];
-fs.readFile("notes.json", "utf8", function readFileCallback(err, data) {
+fs.readFile("db/db.json", "utf8", function readFileCallback(err, data) {
   if (err) {
     console.log(err);
   } else {
@@ -14,23 +16,32 @@ fs.readFile("notes.json", "utf8", function readFileCallback(err, data) {
 });
 
 app.get("/notes", (req, res) => {
-  res.json({
-    messages,
-  });
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
-app.post("/notes", (req, res) => {
+
+app.get("/api/notes", (req, res) => {
+  res.json(messages);
+});
+
+app.post("/api/notes", (req, res) => {
   console.log(req.body);
-  if (!req.body.note) {
+  if (!req.body.title) {
     res.status(400).json({
-      error: "Missing note, JSON must have note property",
+      error: "Missing title, JSON must have title property",
     });
-  } else if (typeof req.body.note !== "string") {
+  } else if (typeof req.body.text !== "string") {
     res.status(400).json({
-      error: "Note must be a string",
+      error: "Note must be a string in the text field",
     });
   } else {
-    messages.push(req.body.note);
-    fs.writeFile("notes.json", JSON.stringify(messages), function (err) {
+    const ids = messages.map((m) => m.id);
+    const uniqueId = messages.length > 0 ? Math.max(...ids) + 1 : 1;
+    messages.push({
+      title: req.body.title,
+      text: req.body.text,
+      id: uniqueId,
+    });
+    fs.writeFile("db/db.json", JSON.stringify(messages), function (err) {
       if (err) {
         console.log(err);
       }
@@ -41,10 +52,20 @@ app.post("/notes", (req, res) => {
   }
 });
 
+app.delete("/api/notes/:id", (req, res) => {
+  messages = messages.filter((note) => note.id !== Number(req.params.id));
+  fs.writeFile("db/db.json", JSON.stringify(messages), function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+  res.json({
+    message: "Note deleted",
+  });
+});
+
 app.get("/", (req, res) => {
-  res.send(
-    `<p>GET /notes to see notes</p><p> POST {"note": "I have a massive database"} to /notes to add a note</p>`
-  );
+  res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(port, () => {
